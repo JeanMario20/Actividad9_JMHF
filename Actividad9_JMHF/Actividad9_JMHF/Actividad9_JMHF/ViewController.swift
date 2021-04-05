c571478fb2f8045289075155eb455633c48cff0e
Jean Mario <jeanmario98763@gmail.com>
2021-04-05 13:33:45 -0500

    actividad9_JMHF

---
+++ Actividad9_JMHF/Actividad9_JMHF/Actividad9_JMHF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Actividad9_JMHF
+//
+//  Created by user172369 on 3/25/21.
+//  Copyright © 2021 user172369. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
